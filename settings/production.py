d57f73d619874504ebea0fcbd6fdadca8253b5d5
Anoop John <anoop21linkdin@gmail.com>
2016-04-19 13:02:03 +0530

    heroku settings in prod

--- settings/production.py
+++ settings/production.py
@@ -1,1 +1,16 @@
 from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+import dj_database_url
+DATABASES = {}
+DATABASES['default'] =  dj_database_url.config()
+
+BASE_URL = "http://app.com/"
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'logging@rawdatatech.com'
+EMAIL_HOST_PASSWORD = 'Abcd123#$'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
