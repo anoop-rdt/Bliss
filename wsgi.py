18da6ac50caf55033bdf9f859b7e44a70a001410
Anoop John <anoop21linkdin@gmail.com>
2016-04-19 13:21:18 +0530

    cling in wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for rdt_base project.
+WSGI config for medicon project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rdt_base.settings")
-
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
