788280cce7551bc961318a2b08dcafcfc91891c1
Anoop John <anoop21linkdin@gmail.com>
2016-04-19 13:28:34 +0530

    registration url removed

--- urls.py
+++ urls.py
@@ -19,7 +19,6 @@ from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name="registration/home.html"), name="home"),
     url(r'^admin/', admin.site.urls),
     url(r'^ble/', include('apps.ble.urls')),
 ]
